381421e1f545916f08f0d412737655f5a224e137
Daniil <twilightnotch@gmail.com>
2023-05-01 18:25:55 +0300

    Update JwtUtils

--- src/main/java/com/example/blps_lab1/config/jwt/JwtUtils.java
+++ src/main/java/com/example/blps_lab1/config/jwt/JwtUtils.java
@@ -1,7 +1,6 @@
 package com.example.blps_lab1.config.jwt;
 
 import com.example.blps_lab1.security.CookUserDetails;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -12,6 +11,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 @Component
@@ -20,19 +23,42 @@ public class JwtUtils {
     private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     @Value("${token.JWTExpirationMS}")
-    private int jwtExpirationMs;
+    private long jwtExpirationMs;
+    @Value("${token.RTExpirationMs}")
+    private long refreshExpirationMS;
 
+    public String generateToken(String login, long time) {
+        Instant now = Instant.now();
+        ZoneId utcZone = ZoneId.of("UTC");
+        ZonedDateTime utcNow = ZonedDateTime.ofInstant(now, utcZone);
+        ZonedDateTime utcExpiration = utcNow.plus(Duration.ofMillis(time));
+        return Jwts.builder()
+                .setSubject(login)
+                .setIssuer("coocker.ru")
+                .setIssuedAt(Date.from(utcNow.toInstant()))
+                .setExpiration(Date.from(utcExpiration.toInstant()))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
 
-    public String generateJwtToken(Authentication authentication) {
+    public String generateJWToken(Authentication authentication) {
         CookUserDetails userPrincipal = (CookUserDetails) authentication.getPrincipal();
-        return generateJwtToken(userPrincipal.getUsername());
+        return generateJWToken(userPrincipal.getUsername());
     }
-    //add kid, issuer, role
-    public String generateJwtToken(String login){
-        return Jwts.builder().setSubject(login).setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(key, SignatureAlgorithm.HS512).compact();
+
+    public String generateJWToken(String login) {
+        return generateToken(login, jwtExpirationMs);
     }
+
+    public String generateRefreshToken(String login) {
+        return generateToken(login, refreshExpirationMS);
+    }
+
+    public String generateRefreshToken(Authentication authentication) {
+        CookUserDetails userPrincipal = (CookUserDetails) authentication.getPrincipal();
+        return generateRefreshToken(userPrincipal.getUsername());
+    }
+
     public boolean validateJwtToken(String jwt) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
